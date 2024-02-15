ae0ed37fda2cac872e1efb64a78d74bff8a09175
Christoffer Näs <116513364+chrnas@users.noreply.github.com>
2024-02-15 09:24:30 +0100

    Create test_my_module.py

---
+++ test_my_module.py
@@ -0,0 +1,17 @@
+"""
+This module provides various tools for testing. 
+It is only a demonstration file and not meant to be used.
+
+Functions:
+    * TestAddFunciton(numbers): Calculates the average of a list of numbers.
+"""
+import unittest
+from my_module import add
+
+class TestAddFunction(unittest.TestCase):
+    """Tests the basic addition functionality of the 'add' function."""
+    def test_add_positive_numbers(self):
+        """
+        Test that the `add` function correctly adds positive numbers.
+        """
+        self.assertEqual(add(2, 3), 5)
