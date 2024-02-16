7bc76b2aa280a1521e73ac89beea738d4f3c2768
Christoffer Näs <116513364+chrnas@users.noreply.github.com>
2024-02-16 09:47:28 +0100

    Update my_module.py

--- my_module.py
+++ my_module.py
@@ -1,24 +1,2 @@
-"""
-This module provides various tools for summation.
-
-Functions:
-    * add(a,b): Perform summation.
-"""
-
-
-def add(a, b):
-    """
-    Adds two numbers together.
-
-    Args:
-        a (int): The first number.
-        b (int): The second number.
-
-    Returns:
-        int: The sum of the two numbers.
-    """
-
-
-
-   
+def add_numbers(a, b):
     return a + b
