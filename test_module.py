16e5bd00e21c655245d219751ec296abdb9b3a88
Christoffer Näs <116513364+chrnas@users.noreply.github.com>
2024-02-19 20:07:42 +0100

    Update test_module.py

--- test_module.py
+++ test_module.py
@@ -9,6 +9,7 @@ Functions:
 import pytest
 from my_module import add
 
+
 def test_add_numbers():
     """
     Test the add_numbers function.
@@ -19,6 +20,7 @@ def test_add_numbers():
     assert add(0, 0) == 0
     assert add(-1, 1) == 0
     assert add(-1, -1) == -2
+
 
 if __name__ == "__main__":
     pytest.main()
