7d38710e090085a245c1d78779477410f7e77b1c
Christoffer Näs <116513364+chrnas@users.noreply.github.com>
2024-02-19 19:46:21 +0100

    Update test_module.py

--- test_module.py
+++ test_module.py
@@ -2,12 +2,12 @@
 This module provides various functions for testing.
 
 Functions:
-    * test_add_numbers: Performs test on the function add.
+    * test_add: Performs test on the function add.
 """
 
 
 import pytest
-from my_module import add_numbers
+from my_module import add
 
 def test_add_numbers():
     """
@@ -15,10 +15,10 @@ def test_add_numbers():
 
     This function checks if the add_numbers function produces the expected results.
     """
-    assert add_numbers(1, 2) == 3
-    assert add_numbers(0, 0) == 0
-    assert add_numbers(-1, 1) == 0
-    assert add_numbers(-1, -1) == -2
+    assert add(1, 2) == 3
+    assert add(0, 0) == 0
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
 
 if __name__ == "__main__":
     pytest.main()
