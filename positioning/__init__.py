789e9d8d749ad6838ad1a6a35a6eacba4fac669d
chris <chrna581@student.liu.se>
2024-02-20 12:49:31 +0100

    changed filenames

--- positioning/__init__.py
+++ positioning/__init__.py
@@ -1,4 +1,4 @@
-from positioning.positioning_handler import pos_methods_data
-from positioning.test_method import method_main
+from .positioning_handler import pos_methods_data
+from .test_method import method_main
 
 
