423cfc896b6b00acef40aceb0fa68a3e55b16ad9
Christoffer Näs <116513364+chrnas@users.noreply.github.com>
2024-03-09 08:25:37 +0100

    Create module.py

---
+++ module.py
@@ -0,0 +1,20 @@
+"""
+This module provides various tools for summation.
+
+Functions:
+    * add(a,b): Perform summation.
+"""
+
+
+def add(a, b):
+    """
+    Adds two numbers together.
+
+    Args:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The sum of the two numbers.
+    """
+    return a + b
