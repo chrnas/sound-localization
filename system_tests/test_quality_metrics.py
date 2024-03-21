a7e4b007d386e30731cf390f60afa249678e7ff4
antonegas <32789589+antonegas@users.noreply.github.com>
2024-03-21 14:52:56 +0100

    Marked tests as skip instead

---
+++ system_tests/test_quality_metrics.py
@@ -0,0 +1,43 @@
+"""
+This file contains the data collection for quality metrics related to the system.
+"""
+
+# TODO: add test id's
+
+import pytest
+
+@pytest.mark.skip # Not implemented
+def test_2d_error():
+    """
+    Quality requirement: 3.5.2
+    Dependencies: This test requires that atleast one method for locating sounds has been implemented.
+    Tests: This test collects metrics on the margin of error when locating sounds in two dimensions.
+    """
+    pass
+
+@pytest.mark.skip # Not implemented
+def test_3d_error():
+    """
+    Quality requirement: 3.5.4
+    Dependencies: This test requires that atleast one method for locating sounds has been implemented. It also requires the system pipeline is implemented in order to correctly measure the delay between data input to data output.
+    Tests: This test collects metrics on the total delay from inputted sounds to outputted position.
+    """
+    print("hello world")
+    pass
+
+@pytest.mark.skip # Not implemented
+def test_delay():
+    """
+    Quality requirement: 3.5.3
+    Dependencies: This test requires that atleast one method for locating sounds has been implemented.
+    Tests: This test collects metrics on the margin of error when locating sounds in three dimensions. This in relation to a horizontal x and y plane, as well as a vertical axis z.
+    """
+    pass
+
+# Helper functions
+
+def distance(source: tuple[float, float, float], prediction: tuple[float, float, float]):
+    """
+    
+    """
+    pass
