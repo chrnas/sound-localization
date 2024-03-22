e5bf75b4065eb93b18c1e52b3ea5602fa6106175
antonegas <32789589+antonegas@users.noreply.github.com>
2024-03-22 12:06:45 +0100

    Added cli systemtests boilerplate

---
+++ system_tests/test_cli_requirements.py
@@ -0,0 +1,75 @@
+"""
+This file contains tests related to the requirements on the CLI.
+"""
+
+def test_developed_cli():
+    """
+    Id: 4
+    CLI requirement: 3.3.1
+    Tests: That a CLI has been implemented to some level.
+    """
+    pass
+
+def test_gives_coordinate():
+    """
+    Id: 5
+    CLI requirement: 3.3.2
+    Tests: That the CLI gives coordinate for a located sound in the form of longitude and latitude.
+    """
+    pass
+
+def test_error_margin():
+    """
+    Id: 6
+    CLI requirement: 3.3.3
+    Tests: That the CLI gives a margin of error after together with the location of the sound.
+    """
+    pass
+
+def test_choose_method():
+    """
+    Id: 7
+    CLI requirement: 3.3.4
+    Tests: That the user can choose between using different types of positioning methods.
+    """
+    pass
+
+def test_add_method():
+    """
+    Id: 8
+    CLI requirement: 3.3.5
+    Tests: That a user can add new postioning methods without access to the source code.
+    """
+    pass
+
+def test_configure_microphones():
+    """
+    Id: 9
+    CLI requirement: 3.3.6
+    Tests: That the user can configure which microphones are used.
+    """
+    pass
+
+def test_choose_ecohandling():
+    """
+    Id: 10
+    CLI requirement: 3.3.7
+    Tests: That the user can choose between different methods for handling of eco.
+    """
+    pass
+
+def test_add_ecohandling():
+    """
+    Id: 11
+    CLI requirement: 3.3.8
+    Tests: That the user can add methods for handling of eco without access to the source code.
+    """
+    pass
+
+def test_combine_methods():
+    """
+    Id: 12
+    CLI requirement: 3.3.9
+    Tests: That the user can choose to use multiple methods for positioning to get a weighted average.
+    """
+    pass
