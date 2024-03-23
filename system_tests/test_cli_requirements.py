3de3ab2ce4205a2231af67962b09f9e5c0b4c5a1
antonegas <32789589+antonegas@users.noreply.github.com>
2024-03-23 16:59:19 +0100

    Added regex to all tests

--- system_tests/test_cli_requirements.py
+++ system_tests/test_cli_requirements.py
@@ -48,7 +48,7 @@ def test_choose_method():
     """
     args = [] # TODO: provide run arguments
     p = subprocess.run(args, capture_output=True)
-    assert re.match(r"Now using (TDOA|AD) to locate sounds", p.stdout.decode()) is not None
+    assert re.match(r"Now using method (TDOA|AD) to locate sounds", p.stdout.decode()) is not None
 
 @pytest.mark.skip # Not implemented
 def test_add_method():
@@ -63,37 +63,73 @@ def test_add_method():
     assert re.match(r"Added method \S+ for locating sounds", p.stdout.decode()) is not None
 
 @pytest.mark.skip # Not implemented
-def test_configure_microphones():
+def test_add_microphones():
     """
-    Id: 9
+    Id: 9.1
     CLI requirement: 3.3.6
     Tests: That the user can configure which microphones are used.
     """
-    pass
+    args = [] # TODO: provide run arguments
+    p = subprocess.run(args, capture_output=True)
+    # TODO: maybe add second test which checks only methods with names without space can be added.
+    assert re.match(r"Added microphone \S+ at -?\d+\.\d+, -?\d+\.\d+(, -?\d+\.\d+)?", p.stdout.decode()) is not None
 
 @pytest.mark.skip # Not implemented
-def test_choose_ecohandling():
+def test_remove_microphones():
+    """
+    Id: 9.2
+    CLI requirement: 3.3.6
+    Tests: That the user can configure which microphones are used.
+    """
+    args = [] # TODO: provide run arguments
+    p = subprocess.run(args, capture_output=True)
+    # TODO: maybe add second test which checks only methods with names without space can be added.
+    assert re.match(r"Removed microphone \S+", p.stdout.decode()) is not None
+
+@pytest.mark.skip # Not implemented
+def test_choose_echohandling():
     """
     Id: 10
     CLI requirement: 3.3.7
-    Tests: That the user can choose between different methods for handling of eco.
+    Tests: That the user can choose between different methods for handling of echo.
     """
-    pass
+    args = [] # TODO: provide run arguments
+    p = subprocess.run(args, capture_output=True)
+    # TODO: maybe add second test which checks only methods with names without space can be added.
+    assert re.match(r"Now using \S+ to handle echos", p.stdout.decode()) is not None
 
 @pytest.mark.skip # Not implemented
-def test_add_ecohandling():
+def test_add_echohandling():
     """
     Id: 11
     CLI requirement: 3.3.8
-    Tests: That the user can add methods for handling of eco without access to the source code.
+    Tests: That the user can add methods for handling of echo without access to the source code.
     """
-    pass
+    args = [] # TODO: provide run arguments
+    p = subprocess.run(args, capture_output=True)
+    # TODO: maybe add second test which checks only methods with names without space can be added.
+    assert re.match(r"Added method \S+ for handling echos", p.stdout.decode()) is not None
 
 @pytest.mark.skip # Not implemented
 def test_combine_methods():
     """
-    Id: 12
+    Id: 12.1
     CLI requirement: 3.3.9
     Tests: That the user can choose to use multiple methods for positioning to get a weighted average.
     """
-    pass
+    args = [] # TODO: provide run arguments
+    p = subprocess.run(args, capture_output=True)
+    # TODO: maybe add second test which checks only methods with names without space can be added.
+    assert re.match(r"Now using methods? \S+((, \S+)? and \S+ together)? to locate sounds", p.stdout.decode()) is not None
+
+@pytest.mark.skip # Not implemented
+def test_weigh_methods():
+    """
+    Id: 12.1
+    CLI requirement: 3.3.9
+    Tests: That the user can set the weight of a method when using multiple methods together.
+    """
+    args = [] # TODO: provide run arguments
+    p = subprocess.run(args, capture_output=True)
+    # TODO: maybe add second test which checks only methods with names without space can be added.
+    assert re.match(r"Set method \S+([sS]'|[^sS]'s) weight to \d+\.\d+", p.stdout.decode()) is not None
