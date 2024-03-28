7e88ce1457ec19cb5604ad4d941d8a927c7a6b8a
davidsjoberg1 <96268076+davidsjoberg1@users.noreply.github.com>
2024-03-28 14:00:51 +0100

    Cleaned up code, finalised tests

--- positioning/test_helpfuncs.py
+++ positioning/test_helpfuncs.py
@@ -1,5 +1,6 @@
 import pytest
 import helpfuncs
+import time
 
 def create_mics(actual_pos, mic_positions):
     distances = [helpfuncs.get_distance(actual_pos, mic) for mic in mic_positions]
@@ -12,50 +13,331 @@ def create_mics(actual_pos, mic_positions):
         mics.append(mic)
     return mics
 
+STEP = 1 # How small steps to take [m]
+EXPANSION = 5 # How much to expand the search area [m]
+TIME_LIMIT = 1.5 # Acceptable time for the function to run [s]
+ACCURACY = 1 # Acceptable difference between actual and calculated position [m]
 
-def test_2d_middle():
+
+def test_1_8():
+    """
+    Testing cases 1-8 from "Datainsamlingsplan 4/4". Coordinates are round to closest 0.5.
+    """
+    mic_positions = [[0, 0], [0, 15], [13, 7.5]]
+    settings = helpfuncs.TravelSettings(len(mic_positions[0]), STEP)
+    settings.smallest_expansion = [EXPANSION for i in settings.smallest_expansion]
+    settings.biggest_expansion = [EXPANSION for i in settings.smallest_expansion]
+
+    start_time = time.time()
+    actual_pos = [0, 0]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [0, 15]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [13, 7.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [4.5, 7.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [10, 0]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [0, 7.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [5, 5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [3, 10]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+
+def test_9_16():
+    """
+    Testing cases 9-16 from "Datainsamlingsplan 4/4". Coordinates are round to closest 0.5.
+    """
+    mic_positions = [[0, 0], [0, 25], [21.5, 12.5]]
+    settings = helpfuncs.TravelSettings(len(mic_positions[0]), STEP)
+    settings.smallest_expansion = [EXPANSION for i in settings.smallest_expansion]
+    settings.biggest_expansion = [EXPANSION for i in settings.smallest_expansion]
+
+    start_time = time.time()
+    actual_pos = [0, 0]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [0, 25]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [21.5, 12.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [7, 12.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [10, 0]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [0, 10]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [4, 11]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [13, 13]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+
+def test_17_24():
+    """
+    Testing cases 17-24 from "Datainsamlingsplan 4/4". Coordinates are round to closest 0.5.
+    """
+    mic_positions = [[0, 0], [0, 35], [30.5, 17.5]]
+    settings = helpfuncs.TravelSettings(len(mic_positions[0]), STEP)
+    settings.smallest_expansion = [EXPANSION for i in settings.smallest_expansion]
+    settings.biggest_expansion = [EXPANSION for i in settings.smallest_expansion]
+
+    start_time = time.time()
+    actual_pos = [0, 0]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [0, 35]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [30.5, 17.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [10, 17.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [10, 0]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [0, 10]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [17.5, 17.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [3, 27]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+
+def test_25_36():
+    """
+    Testing cases 25-36 from "Datainsamlingsplan 4/4". Coordinates are round to closest 0.5.
+    """
+    mic_positions = [[0, 0, 0], [0, 30, 5.5], [30, 30, 0], [30, 0, 5.5]]
+    settings = helpfuncs.TravelSettings(len(mic_positions[0]), STEP)
+    settings.smallest_expansion = [EXPANSION for i in settings.smallest_expansion]
+    settings.biggest_expansion = [EXPANSION for i in settings.smallest_expansion]
+
+    start_time = time.time()
     actual_pos = [15, 15, 0]
-    mic_positions = [[0, 0, 0], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = helpfuncs.find_sound_source(mics)
-    assert resultpos == actual_pos
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
 
-def test_2d_corner1():
-    actual_pos = [0, 0, 0]
-    mic_positions = [[0, 0, 0], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
+    start_time = time.time()
+    actual_pos = [15, 15, 2.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = helpfuncs.find_sound_source(mics)
-    assert resultpos == actual_pos
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
 
-def test_2d_corner2():
+    start_time = time.time()
+    actual_pos = [10, 0, 0]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [10, 0, 2.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
     actual_pos = [0, 30, 0]
-    mic_positions = [[0, 0, 0], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = helpfuncs.find_sound_source(mics)
-    assert resultpos == actual_pos
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
 
-def test_2d_corner3():
-    actual_pos = [30, 30, 0]
-    mic_positions = [[0, 0, 0], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
+    start_time = time.time()
+    actual_pos = [0, 30, 2.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = helpfuncs.find_sound_source(mics)
-    assert resultpos == actual_pos
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
 
-def test_2d_corner4():
-    actual_pos = [30, 0, 0]
-    mic_positions = [[0, 0, 0], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
+    start_time = time.time()
+    actual_pos = [25, 20, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = helpfuncs.find_sound_source(mics)
-    assert resultpos == actual_pos
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
 
-def test_2d_exhaustive():
-    assert 1==1
-    return
-    mic_positions = [[0, 0, 10], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
-    for x in range(30):
-        for y in range(30):
-            actual_pos = [x, y, 0]
-            mics = create_mics(actual_pos, mic_positions)
-            resultpos = helpfuncs.find_sound_source(mics)
-            #resultpos[2] = 0 #Anton kolla inte på denna raden!!!
-            assert resultpos == actual_pos
+    start_time = time.time()
+    actual_pos = [25, 20, 2.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [5, 20, 0]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [5, 20, 2.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [35, 35, 0]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [35, 35, 2.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+def test_5_mics():
+    """
+    Testing some cases with 5 microphones.
+    """
+    mic_positions = [[0, 0, 0], [0, 30, 5.5], [30, 30, 0], [30, 0, 5.5], [15, 15, 0]]
+    settings = helpfuncs.TravelSettings(len(mic_positions[0]), STEP)
+    settings.smallest_expansion = [EXPANSION for i in settings.smallest_expansion]
+    settings.biggest_expansion = [EXPANSION for i in settings.smallest_expansion]
+
+
+    start_time = time.time()
+    actual_pos = [35, 35, 2.5]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [-1, -1, 0]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
+    start_time = time.time()
+    actual_pos = [15, 15, 0]
+    mics = create_mics(actual_pos, mic_positions)
+    resultpos = helpfuncs.find_sound_source(mics, settings)
+    assert sum(resultpos) - sum(actual_pos) <= ACCURACY
+    assert time.time() - start_time < TIME_LIMIT
+
