fed6bef5c1703bf6abd1026cbbc50729f479a6dd
linga754 <linga754@student.liu.se>
2024-03-28 12:40:55 +0100

    Cleaned code

--- positioning/helpfuncs.py
+++ positioning/helpfuncs.py
@@ -8,9 +8,28 @@ class Microphone:
     def __init__(self, distance, coords):
         self.distance = distance
         self.coords = coords
+
+
+class TravelSettings():
+
+    def __init__(self, dimensions, step):
+        self.dimensions = dimensions #Number of dimensions the settings are for
+        self.smallest_start = [0 for i in range(dimensions)] #Smallest coordinates to always include
+        self.biggest_start = [0 for i in range(dimensions)] #Biggest coordinates to always include
+        self.smallest_expansion = [1 for i in range(dimensions)] #How far outside mic area to search in negative coords
+        self.biggest_expansion = [1 for i in range(dimensions)] #How far outside mic area to search in positive coords
+        self.step = step #How far to travel between each search point
     
 
-def get_error(coords, mics):  
+def get_error(coords, mics):
+    """
+    Calculate an error heuristic for a given position in the grid. 
+    See the arcitecture document for an explanation of the error heuristic.
+
+    @param list coords: The coordinates to calculate the error for
+    @params list mics: Instances of the Microphone class
+    @return float: The error of the given position
+    """  
     previous_mic = mics[-1]
     error = 0
     for mic in mics:
@@ -25,129 +44,118 @@ def get_error(coords, mics):
         right_side = mic.distance - previous_mic.distance
         
         error += abs(left_side - right_side)
-
+    
     return error
-
-def get_middle(mics):
-    average_pos = []
-    for axis in range(len(mics[0].coords)):
-        average = 0
-        for mic in mics:
-            average += mic.coords[axis]
-        average /= len(mics)
-        
-        average_pos.append(average)
-
-    return average_pos
     
 
-def travel(mics):
-    curr_best_pos = True
-    no_change = False
-    curr_pos = [15, 15, 30]
-    while not no_change:
-        no_change = True
-        for axis in range(len(curr_pos)):
-            curr_pos[axis] += 1
-            move_positive = get_error(curr_pos, mics)
-            curr_pos[axis] -= 2
-            move_negative = get_error(curr_pos, mics)
-            curr_pos[axis] += 1
-            if move_positive == move_negative:
-                move_dir = 0
-            elif move_positive < move_negative:
-                move_dir = 1
-            else:
-                move_dir = -1
-            
-            while curr_best_pos:
-                next_pos = curr_pos.copy()
-                next_pos[axis] += move_dir 
-                print("CURR: " + str(curr_pos) + " " + str(get_error(curr_pos, mics)))
-                print("NEXT: " + str(next_pos) + " " + str(get_error(next_pos, mics)))
-                if get_error(curr_pos, mics) > get_error(next_pos, mics):
-                    no_change = False
-                    curr_pos = next_pos
-                else:
-                    curr_best_pos = False
+def travel_grid(mics, smallest, biggest, step):
+    """
+    Scan the grid and return the best position of the sound source
+    Gets called on by find_sound_source
 
-            curr_best_pos = True
-    return curr_pos
-
-def travel_lines(mics):
-    smallest = [i for i in mics[0].coords]
-    biggest = [i for i in mics[0].coords]
-
-    for mic in mics:
-        for i in range(len(mic.coords)):
-            if(mic.coords[i] < smallest[i]):
-                smallest[i] = math.floor(mic.coords[i])
-            if(mic.coords[i] > biggest[i]):
-                biggest[i] = math.ceil(mic.coords[i])
-
-    for i in biggest:
-        i += 1
-
-    
-
-def travel_matrix(mics):
-    smallest = [i for i in mics[0].coords]
-    biggest = [i for i in mics[0].coords]
-
-    for mic in mics:
-        for i in range(len(mic.coords)):
-            if(mic.coords[i] < smallest[i]):
-                smallest[i] = math.floor(mic.coords[i])
-            if(mic.coords[i] > biggest[i]):
-                biggest[i] = math.ceil(mic.coords[i])
+    @param list mics: Instances of the Microphone class
+    @param list smallest: The smallest coordinates to search in
+    @param list biggest: The biggest coordinates to search in
+    @param step: The step size to travel in each direction
+    @return list: Guess of the sound source position
+    """
 
     best_error = float("inf")
     best_pos = [i for i in mics[0].coords]
+    positions = [i for i in smallest]
 
-    start_time = time.time()
-    for x in range(smallest[0], biggest[0] + 1):
-        for y in range(smallest[1], biggest[1] + 1):
-            for z in range(smallest[2], biggest[2] + 1):
-                if best_error > get_error((x, y, z), mics):
-                    best_error = get_error((x, y, z), mics)
-                    best_pos = [x, y, z]
+    last_changed = False
+    
+    while True:
+        current_error = get_error(positions, mics)
+        if best_error > current_error:
+            best_pos = positions.copy()
+            best_error = current_error
 
-    print("Loop time: " + str(time.time() - start_time))
+        for i in range(len(positions)):
+            positions[i] += step
+            if positions[i] > biggest[i]:
+                positions[i] = smallest[i]
+            else:
+                break
+
+        if not last_changed and smallest[-1] != positions[-1]:
+            last_changed = True
+        elif last_changed and positions[-1] == smallest[-1]:
+            break
+        
+    return best_pos
+
+
+def find_sound_source(mics, settings):
+    """
+    Sets parameters for the grid search and calls the travel_grid function to 
+    find the best position of the sound source
+
+    @param list mics: Instances of the Microphone class
+    @param TravelSettings settings: The settings for the search
+    @return list: Guess of the sound source position
+    """
+    smallest = [settings.smallest_start[i] for i in range(settings.dimensions)]
+    biggest = [settings.biggest_start[i] for i in range(settings.dimensions)]
+
+    for mic in mics:
+        for i in range(len(mic.coords)):
+            if(mic.coords[i] < smallest[i]):
+                smallest[i] = mic.coords[i]
+            if(mic.coords[i] > biggest[i]):
+                biggest[i] = mic.coords[i]
+
+    for i in range(len(settings.smallest_expansion)):
+        smallest[i] -= settings.smallest_expansion[i]
+        biggest[i] += settings.biggest_expansion[i]
+
+    best_pos = travel_grid(mics, smallest, biggest, settings.step)
     return best_pos 
 
-def get_distance(t1, t2):
+
+def get_distance(sound_pos, mic_pos):
     distance = 0
-    for i in range(len(t1)):
-        distance += (t1[i] - t2[i])**2
+    for i in range(len(sound_pos)):
+        distance += (sound_pos[i] - mic_pos[i])**2
     
     return math.sqrt(distance)
 
+
 if __name__ == "__main__":
+    number_of_mics = input("Please enter the number of microphones: ")
+    mic_positions = []
+    for i in range(int(number_of_mics)):
+        string = input(f"Please enter the coordinates of microphone {i+1}: ")
+        coords = [float(num) for num in string.split(" ")]
+        mic_positions.append(coords)
+
+    step = float(input("Please enter the accuracy: "))
+    settings = TravelSettings(len(mic_positions[0]), step)
+
+    expansion = input("Enter negative search expansion (Default is 1): ")
+    settings.smallest_expansion = [expansion for i in settings.smallest_expansion]
+
+    expansion = input("Enter positive search expansion (Default is 1): ")
+    settings.biggest_expansion = [expansion for i in settings.biggest_expansion]
+
+    string_actual = input("Please enter the coordinates of the actual position: ")
+    actual_pos = [float(num) for num in string_actual.split(" ")]
+
+    
     start_time = time.time()
-    actual_pos = [15, 15, 15]
-
-    mic_positions = [[0, 0, 30], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
+    
     distances = [get_distance(actual_pos, mic) for mic in mic_positions]
-    #for i in range(len(mic_positions))
     first_dist = distances[0]
-
     for dist in distances:
         dist -= first_dist
+    print(distances)
 
     mics = []
-    for i in range(len(mic_positions)):
+    for i in range(int(number_of_mics)):
         mic = Microphone(distances[i], mic_positions[i])
         mics.append(mic)
 
-    resultpos = travel_matrix(mics)
+    resultpos = find_sound_source(mics, settings)
     print(resultpos)
     print("Time: " + str(time.time() - start_time))
-
-    print("Accuracy test:")
-    """
-    for x in range(30):
-        for y in range(30):
-            print("(" + str(y) + " " + str(round(get_error((x, y, 0), mics), 1)), end = ") ")
-        print(x)
-    """
-
--- positioning/test_helpfuncs.py
+++ positioning/test_helpfuncs.py
@@ -17,35 +17,45 @@ def test_2d_middle():
     actual_pos = [15, 15, 0]
     mic_positions = [[0, 0, 0], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = helpfuncs.travel(mics)
+    resultpos = helpfuncs.find_sound_source(mics)
     assert resultpos == actual_pos
 
 def test_2d_corner1():
     actual_pos = [0, 0, 0]
     mic_positions = [[0, 0, 0], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = helpfuncs.travel(mics)
+    resultpos = helpfuncs.find_sound_source(mics)
     assert resultpos == actual_pos
 
 def test_2d_corner2():
     actual_pos = [0, 30, 0]
     mic_positions = [[0, 0, 0], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = helpfuncs.travel(mics)
+    resultpos = helpfuncs.find_sound_source(mics)
     assert resultpos == actual_pos
 
 def test_2d_corner3():
     actual_pos = [30, 30, 0]
     mic_positions = [[0, 0, 0], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = helpfuncs.travel(mics)
+    resultpos = helpfuncs.find_sound_source(mics)
     assert resultpos == actual_pos
 
 def test_2d_corner4():
     actual_pos = [30, 0, 0]
     mic_positions = [[0, 0, 0], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = helpfuncs.travel(mics)
+    resultpos = helpfuncs.find_sound_source(mics)
     assert resultpos == actual_pos
 
-
+def test_2d_exhaustive():
+    assert 1==1
+    return
+    mic_positions = [[0, 0, 10], [0, 30, 0], [30, 30, 0], [30, 0, 0]]
+    for x in range(30):
+        for y in range(30):
+            actual_pos = [x, y, 0]
+            mics = create_mics(actual_pos, mic_positions)
+            resultpos = helpfuncs.find_sound_source(mics)
+            #resultpos[2] = 0 #Anton kolla inte på denna raden!!!
+            assert resultpos == actual_pos
