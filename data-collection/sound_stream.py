5bd4164ec4a61bb564fde1d2f9cba611875dc589
mathias <mathias>
2024-04-16 12:57:16 +0200

    sounddevice

--- data-collection/sound_stream.py
+++ data-collection/sound_stream.py
@@ -6,7 +6,7 @@ import os
 import zlib
 import sys
 import ntplib
-
+import sounddevice as sd
 
 ARGS = sys.argv
 # IP = str(ARGS[0])
@@ -26,7 +26,7 @@ CHUNK = 1024
 RECORD_SECONDS = 7
 OUTPUT_FOLDER = "output_local"
 
-audio = pyaudio.PyAudio()
+# audio = pyaudio.PyAudio()
 
 frames = []
 
@@ -44,25 +44,28 @@ def record_audio(start_time):
     while ntp_time < start_time:
         ntp_time = ntp_time + time.perf_counter() - init_time
 
-    stream = audio.open(format=FORMAT, channels=CHANNELS,
-                        rate=RATE, input=True, frames_per_buffer=CHUNK)
-    clear_buffer(stream)  # Clear buffer before recording
+    # stream = audio.open(format=FORMAT, channels=CHANNELS,
+        # rate=RATE, input=True, frames_per_buffer=CHUNK)
+    # clear_buffer(stream)  # Clear buffer before recording
     start_recording = time.perf_counter()
 
-    while time.perf_counter() - start_recording < RECORD_SECONDS:
-        data = stream.read(CHUNK)
-        frames.append(data)
-    print(
-        f"Recording finished. Time: {time.perf_counter() - start_recording} seconds.")
+    data = sd.rec(int(RATE * RECORD_SECONDS), samplerate=RATE,
+                  channels=CHANNELS, dtype='int16')
+    sd.wait()  # Wait until recording is finished
+    # while time.perf_counter() - start_recording < RECORD_SECONDS:
+    #    data = stream.read(CHUNK)
+    #    frames.append(data)
+    # print(
+    #    f"Recording finished. Time: {time.perf_counter() - start_recording} seconds.")
 
     # print("Recording finished.")
 
     # Stop and close the stream
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-
-    return b''.join(frames), ntp_time
+    # stream.stop_stream()
+    # stream.close()
+    # audio.terminate()
+#
+    return data, ntp_time  # b''.join(frames), ntp_time
 
 
 def save_wav(test_id, timestamp, data):
