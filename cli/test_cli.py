db12ad599efb8a002b3539e248f4f8df22eb420c
davidsjoberg1 <96268076+davidsjoberg1@users.noreply.github.com>
2024-04-17 20:57:24 +0200

    started testing

--- cli/test_cli.py
+++ cli/test_cli.py
@@ -2,6 +2,7 @@
 #import pytest
 import subprocess
 import json
+import os
 
 import Positioning as pos
 import ecohandling as eco
@@ -10,13 +11,19 @@ from cli import main_loop
 ## BEHÖVER HJÄLP..
 
 def test_add_mic():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../Positioning/microphones.json')
+    user_input = 'MicRemove all'.encode()
+    subprocess.run(["python3", "main.py"], input=user_input, capture_output=True)
+
     user_input = 'MicAdd 1 2 3'.encode()
     subprocess.run(["python3", "main.py"], input=user_input, capture_output=True)
-    with open("Positioning/microphones.json", "r") as infile:
+    with open(filename, "r") as infile:
         file = json.load(infile)
     assert file == [{'name': '1', 'latitude': 2.0, 'longitude': 3.0}]
+ 
 
-    
+    """
     user_input = 'MicAdd 2 3 4'.encode()
     subprocess.run(["python3", "main.py"], input=user_input, capture_output=True)
     with open("Positioning/microphones.json", "r") as infile:
@@ -24,23 +31,10 @@ def test_add_mic():
     
     assert file == [{'name': '1', 'latitude': 2.0, 'longitude': 3.0},
                     {'name': '2', 'latitude': 3.0, 'longitude': 4.0}]
-    
-    
-
+    """
     
 
-def test_pos_list():
-    user_input = 'MicList\n'.encode()
-    res = subprocess.run(["python3", "main.py"], input=user_input, capture_output=True)
-    
 
-    res = res.stdout.decode().split("\n")
-    res = res[2:]
-    print(res)
-    #main_loop.handle_command(["PosList"], pos.pos_methods_data, eco.eco_methods_data)
-    
-
-  
 
 
 
