97ba98d370111beefadf8c59afb6a3794602555f
antonegas <32789589+antonegas@users.noreply.github.com>
2024-04-22 16:29:06 +0200

    Flake8

--- system_tests/test_cli_requirements.py
+++ system_tests/test_cli_requirements.py
@@ -21,7 +21,8 @@ def test_developed_cli(args):
     """
     Id: 4
     CLI requirement: 3.3.1
-    Tests: That a CLI has been implemented to some level (it has atleast one command).
+    Tests: That a CLI has been implemented to some level (it has atleast one
+    command).
     """
     p = subprocess.run(args)
     assert p.returncode == 0
@@ -36,14 +37,16 @@ def test_gives_coordinate(args):
     """
     Id: 5
     CLI requirement: 3.3.2
-    Tests: That the CLI gives coordinate for a located sound in the form of longitude and latitude.
+    Tests: That the CLI gives coordinate for a located sound in the form of
+    longitude and latitude.
     """
     p = subprocess.run(args, capture_output=True)
     # Example pass: Possible location -777.777, 888.0
     # Example fail: Possible location -777.777 888.0
     # Example fail: Possible location -777., 888.0
-    assert re.fullmatch(
-        r"Possible location -?\d+\.\d+, -?\d+\.\d+", p.stdout.decode()) is not None
+    assert (re.fullmatch(
+        r"Possible location -?\d+\.\d+, -?\d+\.\d+", p.stdout.decode())
+        is not None)
 
 
 @pytest.mark.skip  # Not implemented
@@ -55,7 +58,8 @@ def test_error_margin(args):
     """
     Id: 6
     CLI requirement: 3.3.3
-    Tests: That the CLI gives a margin of error after together with the location of the sound.
+    Tests: That the CLI gives a margin of error after together with the
+    location of the sound.
     """
     p = subprocess.run(args, capture_output=True)
     # Example pass: Margin of error: 123.123 meters
@@ -63,7 +67,8 @@ def test_error_margin(args):
     # Example fail: Margin of error: 123.123 meter
     # Example fail: Margin of error: 1.0 meters
     assert re.fullmatch(
-        r"Margin of error: (1\.0+ meter|(?!1\.0+)\d+\.\d+ meters)", p.stdout.decode()) is not None
+        r"Margin of error: (1\.0+ meter|(?!1\.0+)\d+\.\d+ meters)",
+        p.stdout.decode()) is not None
 
 
 @pytest.mark.skip  # Not implemented
@@ -75,15 +80,17 @@ def test_choose_method(args):
     """
     Id: 7
     CLI requirement: 3.3.4
-    Tests: That the user can choose between using different types of positioning methods.
+    Tests: That the user can choose between using different types of
+    positioning methods.
     """
     p = subprocess.run(args, capture_output=True)
     # Assuming no other methods for locating sounds has been added.
     # Example pass: Now using method TDOA to locate sounds
     # Example pass: Now using method AD to locate sounds
     # Example fail: Now using
-    assert re.fullmatch(
-        r"Now using method (TDOA|AD) to locate sounds", p.stdout.decode()) is not None
+    assert (re.fullmatch(
+        r"Now using method (TDOA|AD) to locate sounds", p.stdout.decode())
+        is not None)
 
 
 @pytest.mark.skip  # Not implemented
@@ -95,7 +102,8 @@ def test_add_method(args):
     """
     Id: 8
     CLI requirement: 3.3.5
-    Tests: That a user can add new postioning methods without access to the source code.
+    Tests: That a user can add new postioning methods without access to the
+    source code.
     """
     p = subprocess.run(args, capture_output=True)
     # Example pass: Added method AAA for locating sounds
@@ -120,7 +128,8 @@ def test_add_microphones(args):
     # Example pass: Added microphone BBB at -888.0, 777.777
     # Example fail: Added microphone CCC at -888.0, 777.777 555.0
     assert re.fullmatch(
-        r"Added microphone \S+ at -?\d+\.\d+, -?\d+\.\d+(, -?\d+\.\d+)?", p.stdout.decode()) is not None
+        r"Added microphone \S+ at -?\d+\.\d+, -?\d+\.\d+(, -?\d+\.\d+)?",
+        p.stdout.decode()) is not None
 
 
 @pytest.mark.skip  # Not implemented
@@ -151,7 +160,8 @@ def test_choose_echohandling(args):
     """
     Id: 11
     CLI requirement: 3.3.7
-    Tests: That the user can choose between different methods for handling of echo.
+    Tests: That the user can choose between different methods for handling of
+    echo.
     """
     p = subprocess.run(args, capture_output=True)
     # Example pass: Now using AAA to handle echos
@@ -169,7 +179,8 @@ def test_add_echohandling(args):
     """
     Id: 12
     CLI requirement: 3.3.8
-    Tests: That the user can add methods for handling of echo without access to the source code.
+    Tests: That the user can add methods for handling of echo without access
+    to the source code.
     """
     p = subprocess.run(args, capture_output=True)
     # Example pass: Added method AAA for handling echos
@@ -187,17 +198,20 @@ def test_combine_methods(args):
     """
     Id: 13
     CLI requirement: 3.3.9
-    Tests: That the user can choose to use multiple methods for positioning to get a weighted average.
+    Tests: That the user can choose to use multiple methods for positioning to
+    get a weighted average.
     """
     p = subprocess.run(args, capture_output=True)
     # NOTE: missing s after method not caught
     # Example pass: Now using method AAA to locate sounds
     # Example pass: Now using method AAA and BBB together to locate sounds
-    # Example pass: Now using methods AAA, BBB and CCC together to locate sounds
+    # Example pass: Now using methods AAA, BBB and CCC together to locate
+    # sounds
     # Example fail: Now using methods AAA, and BBB together to locate sounds
     # Example fail: Now using method AAA together to locate sounds
     assert re.fullmatch(
-        r"Now using methods? \S+((, \S+)? and \S+ together)? to locate sounds", p.stdout.decode()) is not None
+        r"Now using methods? \S+((, \S+)? and \S+ together)? to locate sounds",
+        p.stdout.decode()) is not None
 
 
 @pytest.mark.skip  # Not implemented
@@ -209,7 +223,8 @@ def test_weigh_methods(args):
     """
     Id: 14
     CLI requirement: 3.3.9
-    Tests: That the user can set the weight of a method when using multiple methods together.
+    Tests: That the user can set the weight of a method when using multiple
+    methods together.
     """
     p = subprocess.run(args, capture_output=True)
     # Example pass: Set method AAA's weight to 1.1
@@ -217,4 +232,5 @@ def test_weigh_methods(args):
     # Example fail: Set method AAA' weight to 1.1
     # Example fail: Set method SSS's weight to 2.2
     assert re.fullmatch(
-        r"Set method \S+([sS]'|[^sS]'s) weight to \d+\.\d+", p.stdout.decode()) is not None
+        r"Set method \S+([sS]'|[^sS]'s) weight to \d+\.\d+",
+        p.stdout.decode()) is not None
