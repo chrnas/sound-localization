476f1a9ced0586aca2f4d1b67c49ed3a27bcab0a
linga754 <linga754@student.liu.se>
2024-04-23 11:37:55 +0200

    testfixes

--- positioning/calcfunctions/test_gridtrilaterate.py
+++ positioning/calcfunctions/test_gridtrilaterate.py
@@ -1,4 +1,5 @@
-from . helpfuncs import get_distance, Microphone, TravelSettings, find_sound_source
+from .gridtrilaterate import get_distance, GridTravelSettings, trilaterate_grid
+from . import reciever
 import time
 
 
@@ -8,10 +9,9 @@ def create_mics(actual_pos, mic_positions):
     first_dist = distances[0]
     for dist in distances:
         dist -= first_dist
-    mics = []
+    mics = reciever.create_mics(mic_positions)
     for i in range(len(mic_positions)):
-        mic = Microphone(distances[i], mic_positions[i])
-        mics.append(mic)
+        mics[i].set_distance_difference(distances[i])
     return mics
 
 
@@ -27,7 +27,7 @@ def test_1_8():
     Testing cases 1-8 from "Datainsamlingsplan 4/4". Coordinates are round to closest 0.5.
     """
     mic_positions = [[0, 0], [0, 15], [13, 7.5]]
-    settings = TravelSettings(len(mic_positions[0]), STEP)
+    settings = GridTravelSettings(len(mic_positions[0]), STEP)
     settings.smallest_expansion = [
         EXPANSION for i in settings.smallest_expansion]
     settings.biggest_expansion = [
@@ -36,56 +36,56 @@ def test_1_8():
     start_time = time.time()
     actual_pos = [0, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [0, 15]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [13, 7.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [4.5, 7.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [10, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [0, 7.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [5, 5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [3, 10]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
@@ -95,7 +95,7 @@ def test_9_16():
     Testing cases 9-16 from "Datainsamlingsplan 4/4". Coordinates are round to closest 0.5.
     """
     mic_positions = [[0, 0], [0, 25], [21.5, 12.5]]
-    settings = TravelSettings(len(mic_positions[0]), STEP)
+    settings = GridTravelSettings(len(mic_positions[0]), STEP)
     settings.smallest_expansion = [
         EXPANSION for i in settings.smallest_expansion]
     settings.biggest_expansion = [
@@ -104,56 +104,56 @@ def test_9_16():
     start_time = time.time()
     actual_pos = [0, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [0, 25]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [21.5, 12.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [7, 12.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [10, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [0, 10]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [4, 11]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [13, 13]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
@@ -163,7 +163,7 @@ def test_17_24():
     Testing cases 17-24 from "Datainsamlingsplan 4/4". Coordinates are round to closest 0.5.
     """
     mic_positions = [[0, 0], [0, 35], [30.5, 17.5]]
-    settings = TravelSettings(len(mic_positions[0]), STEP)
+    settings = GridTravelSettings(len(mic_positions[0]), STEP)
     settings.smallest_expansion = [
         EXPANSION for i in settings.smallest_expansion]
     settings.biggest_expansion = [
@@ -172,56 +172,56 @@ def test_17_24():
     start_time = time.time()
     actual_pos = [0, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [0, 35]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [30.5, 17.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [10, 17.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [10, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [0, 10]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [17.5, 17.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [3, 27]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
@@ -231,7 +231,7 @@ def test_25_36():
     Testing cases 25-36 from "Datainsamlingsplan 4/4". Coordinates are round to closest 0.5.
     """
     mic_positions = [[0, 0, 0], [0, 30, 5.5], [30, 30, 0], [30, 0, 5.5]]
-    settings = TravelSettings(len(mic_positions[0]), STEP)
+    settings = GridTravelSettings(len(mic_positions[0]), STEP)
     settings.smallest_expansion = [
         EXPANSION for i in settings.smallest_expansion]
     settings.biggest_expansion = [
@@ -240,84 +240,84 @@ def test_25_36():
     start_time = time.time()
     actual_pos = [15, 15, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [15, 15, 2.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [10, 0, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [10, 0, 2.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [0, 30, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [0, 30, 2.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [25, 20, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [25, 20, 2.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [5, 20, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [5, 20, 2.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [35, 35, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [35, 35, 2.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
@@ -328,7 +328,7 @@ def test_5_mics():
     """
     mic_positions = [[0, 0, 0], [0, 30, 5.5], [
         30, 30, 0], [30, 0, 5.5], [15, 15, 0]]
-    settings = TravelSettings(len(mic_positions[0]), STEP)
+    settings = GridTravelSettings(len(mic_positions[0]), STEP)
     settings.smallest_expansion = [
         EXPANSION for i in settings.smallest_expansion]
     settings.biggest_expansion = [
@@ -337,20 +337,20 @@ def test_5_mics():
     start_time = time.time()
     actual_pos = [35, 35, 2.5]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [-1, -1, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
 
     start_time = time.time()
     actual_pos = [15, 15, 0]
     mics = create_mics(actual_pos, mic_positions)
-    resultpos = find_sound_source(mics, settings)
+    resultpos = trilaterate_grid(mics, settings)
     assert sum(resultpos) - sum(actual_pos) <= ACCURACY
     assert time.time() - start_time < TIME_LIMIT
