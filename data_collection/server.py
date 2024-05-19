54445fed9805346ed1cd03a9f402996b36f70ea3
mathias <mathias>
2024-05-19 19:20:57 +0200

    save

--- data_collection/server.py
+++ data_collection/server.py
@@ -13,15 +13,30 @@ import pyaudio
 import zlib
 import ntplib
 
-perf_counter = time.perf_counter()
+from positioning.calcfunctions.receiver import Receiver
+from positioning.calcfunctions import constants
+from positioning.tdoa import calculate_time_differences
+from positioning.TidsförskjutningBeräkning import calc_offset_from_samples
+
+
+initial_time = time.perf_counter()
+ntp_client = ntplib.NTPClient()
+response = ntp_client.request('pool.ntp.org', version=3)
+
+last_loud_noise_time = 0
+loud_noise_cooldown = 2
 
 
 class Microphone:
-    def __init__(self, id, sample_rate):
+    def __init__(self, id, sample_rate, latitude, longitude):
         self.id = id
         self.sample_rate = sample_rate
         self.current_audio_data = b""
         self.current_timestamp = 0
+        self.latitude = latitude
+        self.longitude = longitude
+        # Track if data has been submitted since the last loud noise
+        self.data_submitted = False
 
     def set_test_id(self, id):
         self.test_id = id
@@ -68,31 +83,26 @@ microphones: dict[int, Microphone] = {}
 socketio = SocketIO(app, max_http_buffer_siz=1e10)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        test_id = request.form['test_id']
-        # Do something with the input_text, for example, print it
-        print("Input text:", test_id)
-        start_test(test_id)
-    return render_template('index.html', name='app')
+@socketio.on('loudNoise')
+def handle_loud_noise():
+    global last_loud_noise_time, response, initial_time
+    current_time = time.perf_counter()
+    ntp_current_time = response.tx_time + (current_time - initial_time)
 
+    if ntp_current_time - last_loud_noise_time > loud_noise_cooldown:
+        last_loud_noise_time = ntp_current_time
+        future_timestamp = ntp_current_time + 2.5
+        emit('start_test', {'test_id': "test",
+             'start_time': future_timestamp}, broadcast=True)
 
-# TODO Fix this for NTP
-@app.route('/start_test/<test_id>')
-def start_test(test_id):
-    print('Test ID:', test_id)
-    # Get current server time
+        # Reset data submission flag for all microphones
+        for mic in microphones.values():
+            mic.data_submitted = False
 
-    ntp_client = ntplib.NTPClient()
-    response = ntp_client.request('pool.ntp.org', version=3)
-    current_time = response.tx_time
-    future_timestamp = current_time + 2
-
-    emit('start_test', {'test_id': test_id,
-         'start_time': future_timestamp}, broadcast=True, namespace="/")
-
-    return {"msg": f"starting test {test_id} at {future_timestamp}"}
+        print(
+            f"Loud noise processed, emitting start test event for future timestamp: {future_timestamp}")
+    else:
+        print("Loud noise ignored due to server-wide cooldown.")
 
 
 @socketio.on('newMicrophone')
@@ -101,10 +111,13 @@ def handle_new_microphone(data):
 
     microphone_id = data.get('id') or 0
     sample_rate = data.get('sample_rate') or 44100
+    latitude = data.get('latitude') or 0
+    longitude = data.get('longitude') or 0
+    microphones[request.sid] = Microphone(
+        microphone_id, sample_rate, latitude, longitude)
 
-    microphones[request.sid] = Microphone(microphone_id, sample_rate)
-
-    print(f'New microphone connected: {microphone_id}')
+    print(
+        f'New microphone connected: {microphone_id} with sample rate {sample_rate} and coordinates ({latitude}, {longitude})')
 
 
 @socketio.on('audioData')
@@ -119,21 +132,32 @@ def handle_audio_data(data):
 @socketio.on('endOfData')
 def save_data(test_id):
     microphone = microphones[request.sid]
-    print("saving...")
-    thread = Thread(target=microphone.save, args=(test_id,))
-    thread.start()
-    # microphone.save(test_id)
+    test_id = 1  # Example test ID, replace with real logic if necessary
+    if not microphone.data_submitted:
+        print("Saving...")
+        thread = Thread(target=microphone.save, args=(test_id,))
+        thread.start()
+        microphone.data_submitted = True
+        # Check if all microphones have submitted their data
+        if all(mic.data_submitted for mic in microphones.values()):
+            print("All microphones have submitted their data.")
+
+            # Triangulate the source of the sound
+
+    else:
+        print("Data for this microphone has already been submitted)")
 
 
-# TODO Old stuff
+@ socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+    if request.sid in microphones:
+        del microphones[request.sid]
 
 
-@socketio.on('syncTime')
-def handle_sync_time():
-    # Function to handle sync time request from the client
-    print("Sync requested")
-    server_timestamp = time.perf_counter() - perf_counter
-    emit('syncResponse', server_timestamp)
+def triangulate():
+    # TODO Implement triangulation logic
+    pass
 
 
 if __name__ == '__main__':
